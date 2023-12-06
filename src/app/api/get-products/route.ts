d72b984a754bcc311807cab9d44b1f6b3b30e32d
algomachine007 <benneth@bamble.io>
2023-12-06 07:36:52 +0100

    ensuring sorting and filtering works

--- src/app/api/get-products/route.ts
+++ src/app/api/get-products/route.ts
@@ -7,61 +7,59 @@ import { ErrorHandler } from "../helpers/error-handler";
 export const GET = async (req: NextRequest, res: NextResponse) => {
   await dbConnect();
 
-  // filtration and search logic
-  const url = new URL(req.url);
-  const searchParams = new URLSearchParams(url.search);
-  const category = searchParams.get("category");
-  const product = searchParams.get("product");
-  const sortBy = searchParams.get("sortBy");
-  const direction = searchParams.get("direction");
-
-  let query: any;
-  let filterByCategories;
-
-  if (
-    category?.length !== 0 ||
-    (sortBy?.length === 0 && direction?.length === 0)
-  ) {
-    const isMultiple = category?.split(",").length > 1;
-    filterByCategories = isMultiple ? category?.split(",") : category;
-    // filtering by category
-    query = ProductModel.find({
-      category: filterByCategories,
-    });
-  }
-  // else if (category?.length && sortBy && direction) {
-  //   query = ProductModel.find().sort({
-  //     [`${sortBy}`]: direction,
-  //   });
-
-  // filtering by title i.e search
-  else if (product?.length && typeof product !== null) {
-    query = ProductModel.find({
-      title: product,
-    });
-  } else {
-    query = ProductModel.find();
-  }
-
   // sort logic: //title, price
-
-  // if (sortBy && direction) {
-  //   query = ProductModel.find().sort({
-  //     [`${sortBy}`]: direction,
-  //   });
-  // }
 
   // build the model with the query conditionally then finally execute
   // http://localhost:3000/api/get-products?category=smartphones&sortBy=title&direction=asc
-  if (category?.length && sortBy && direction) {
-    query = ProductModel.find({
-      category: filterByCategories,
-    }).sort({
-      [`${sortBy}`]: direction,
-    });
-  }
+  // if (category?.length && sortBy?.length && direction?.length) {
+  //   query = ProductModel.find({
+  //     category: filterByCategories,
+  //   }).sort({
+  //     sortBy: direction?.toLowerCase(),
+  //   });
+  // }
 
   try {
+    // filtration and search logic
+    const url = new URL(req.url);
+    const searchParams = new URLSearchParams(url.search);
+    const category = searchParams.get("category");
+    const product = searchParams.get("product");
+    const sortBy = searchParams.get("sortBy");
+    const direction = searchParams.get("direction");
+
+    const hasCategoryWithoutSort = Boolean(
+      category?.length && !sortBy?.length && !direction?.length,
+    );
+    const hasCategoryWithSort = Boolean(
+      category?.length && sortBy?.length && direction?.length,
+    );
+
+    let query;
+    let filterByCategories;
+    const isMultiple = category?.split(",").length! > 1;
+    filterByCategories = isMultiple ? category?.split(",") : category;
+    if (hasCategoryWithoutSort) {
+      // filtering by category
+      query = ProductModel.find({
+        category: filterByCategories,
+      });
+    } else if (hasCategoryWithSort) {
+      query = ProductModel.find({
+        category: filterByCategories,
+      }).sort({
+        [`${sortBy}`]: direction?.toLowerCase(),
+      });
+    }
+    // filtering by title i.e search
+    else if (product?.length) {
+      query = ProductModel.find({
+        title: product,
+      });
+    } else {
+      query = ProductModel.find();
+    }
+
     const products: Awaited<TProduct[]> = await query.exec();
 
     return NextResponse.json(
@@ -73,6 +71,22 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
       },
     );
   } catch (error) {
+    console.log("ERROR", error);
+
+    if (error instanceof Error) {
+      if (String(error).includes("TypeError")) {
+        return NextResponse.json(
+          {
+            message: String(error),
+          },
+          {
+            status: 401,
+            statusText: String(error),
+          },
+        );
+      }
+    }
+
     return ErrorHandler("Something went wrong", 500);
   }
 };
