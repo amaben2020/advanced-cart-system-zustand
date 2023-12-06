34124e8dd8e3042a797aec1269ff115dfc027772
algomachine007 <benneth@bamble.io>
2023-12-06 11:27:52 +0100

    ensuring the filters are dropdowns

--- src/components/elements/sidebar/index.tsx
+++ src/components/elements/sidebar/index.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
+const FilterDropdown = ({ title }: { title: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => setIsOpen((p) => !p)}
+        className="w-full p-6 transition rounded-lg shadow-sm shadow-green-700"
+      >
+        {title}
+      </button>
+
+      {isOpen && <div>SELECTS</div>}
+    </>
+  );
+};
+
 const Sidebar = ({
   categories,
   brands,
@@ -9,34 +27,24 @@ const Sidebar = ({
       {/* TODO: use a dropdown for this, the dummy api doesnt support this */}
       <p> Filters </p>
 
+      <FilterDropdown title="Category" />
+
       <div className="flex flex-col gap-3 mb-4">
         <p>By Category</p>
-        {/* The UI should be a dropdown */}
+
         <div className="flex flex-col gap-3">
           {categories.map((category: string) => (
-            <>
-              <p> {category} </p>
+            <div className="flex items-center gap-x-4" key={category}>
               <input
                 // checked={category === selectedCategory}
                 onChange={() => handleCategoryFilter(category)}
                 type="checkbox"
                 className="w-5 h-5 cursor-pointer accent-green-700"
               />
-            </>
+              <p> {category} </p>
+            </div>
           ))}
         </div>
-
-        {/* <div className="flex flex-col gap-3">
-          {brands.map((brand: string) => (
-            <>
-              <p> {brand} </p>
-              <input
-                type="checkbox"
-                className="w-5 h-5 cursor-pointer accent-green-700"
-              />
-            </>
-          ))}
-        </div> */}
       </div>
     </aside>
   );
