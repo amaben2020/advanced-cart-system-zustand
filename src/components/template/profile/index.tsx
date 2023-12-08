0c1012779e653b7a490618ee333d3f6c3136be93
algomachine007 <benneth@bamble.io>
2023-12-08 16:55:02 +0100

    adding back button to profile

--- src/components/template/profile/index.tsx
+++ src/components/template/profile/index.tsx
@@ -3,6 +3,7 @@
 import useDataFetch from "@/hooks/useDataFetch";
 import { generateAvatar } from "@/utils/generateAvatar";
 import { useSession } from "next-auth/react";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 // to use server components and enforce auto code splitting, write all use client code elsewhere and simply import in server component
@@ -25,9 +26,8 @@ const ProfileTemplate = () => {
     //@ts-ignore
   }>(`/user/get-user?email=${session.data?.user?.user.email}`);
 
-  console.log(state);
-
-  if (session.status === "authenticated") {
+  //@ts-ignore
+  if (session?.status === "authenticated" && session?.data?.user?.user) {
     const { text, bg, textColor } = generateAvatar(
       //@ts-ignore
       session.data?.user.user?.firstName ?? "",
@@ -38,7 +38,25 @@ const ProfileTemplate = () => {
     return (
       <section>
         <div className="relative p-20 bg-black">
-          Upper, avatar generated with initials
+          <div className="w-[50px]">
+            <Link href="/">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 text-white"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+                />
+              </svg>
+            </Link>
+          </div>
+
           <div
             style={{
               background: bg,
@@ -66,10 +84,10 @@ const ProfileTemplate = () => {
             </thead>
             <tbody>
               <tr>
-                <td>{state.data.user.firstName}</td>
-                <td>{state.data.user.lastName}</td>
-                <td>{state.data.user.role}</td>
-                <td>{state.data.user.email}</td>
+                <td>{state?.data?.user?.firstName}</td>
+                <td>{state?.data?.user?.lastName}</td>
+                <td>{state?.data?.user?.role}</td>
+                <td>{state?.data?.user?.email}</td>
               </tr>
             </tbody>
           </table>
