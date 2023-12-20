cf8115c3b7b25cc18d07edaf50341102dbba881e
algomachine007 <benneth@bamble.io>
2023-12-20 11:39:07 +0100

    removing caching strategy

--- src/app/api/get-products/route.ts
+++ src/app/api/get-products/route.ts
@@ -6,19 +6,10 @@ import { escapeRegex } from "@/utils/fuzzySearch";
 import { NextRequest, NextResponse } from "next/server";
 import { ErrorHandler } from "../helpers/error-handler";
 
-import { Redis } from "@upstash/redis";
-
-const redis = Redis.fromEnv();
-
 export const GET = async (req: NextRequest, res: NextResponse) => {
   await dbConnect();
   const REDIS_KEY = "cache";
   try {
-    let result: any = {};
-    const cachedData = await redis.get(REDIS_KEY);
-
-    console.log(cachedData);
-
     // filtration and search logic
     const { category, product, sortBy, direction } = extractSearchParams(
       req.url,
@@ -100,28 +91,3 @@ export const GET = async (req: NextRequest, res: NextResponse) => {
     return ErrorHandler("Something went wrong", 500);
   }
 };
-
-// if (cachedData) {
-//   result.data = cachedData;
-//   result.latency = Date.now() - Date.now();
-//   result.type = "cache";
-//   console.log(result);
-//   return NextResponse.json({
-//     data: result,
-//     message: "Success",
-//   });
-// } else {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   const data = await res.json();
-
-//   await redis.set(REDIS_KEY, JSON.stringify(data), { ex: 60 });
-
-//   result.data = data;
-//   result.latency = Date.now() - Date.now();
-//   result.type = "api";
-//   console.log("Making fresh api call");
-//   return NextResponse.json({
-//     data: result,
-//     message: "Success",
-//   });
-// }
