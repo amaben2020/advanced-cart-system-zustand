4e95163a2deef9424b6358e961e7e25bc7aff68d
algomachine007 <benneth@bamble.io>
2023-12-23 10:04:25 +0100

    creating admin create page

--- src/app/(pages)/admin/create/page.tsx
+++ src/app/(pages)/admin/create/page.tsx
@@ -1,13 +1,37 @@
+import Input from "@/components/elements/input";
 import AdminDashboardLayout from "../layout";
 
 const CreateProduct = () => {
   return (
     <AdminDashboardLayout>
-      <h1>Create Product</h1>
+      <section className="flex flex-col gap-5 p-10">
+        <h1>Create Product</h1>
 
-      <form action="">
-        <input type="text" />
-      </form>
+        <form action="">
+          <div className="flex flex-wrap items-center gap-6 my-4">
+            <Input label="title" type="text" className="flex-1" />
+            <Input label="description" type="text" />
+            <Input label="price" type="number" />
+            <Input
+              label="discountPercentage"
+              type="number"
+              className="flex-1"
+            />
+          </div>
+
+          <Input label="rating" type="number" />
+          <Input label="stock" type="number" />
+          <Input label="brand" type="text" />
+          <Input label="category" type="text" />
+          <Input label="thumbnail" type="text" />
+          {/* // array of strings */}
+          <Input label="images" type="text" />
+
+          <button className="block" type="submit">
+            Create
+          </button>
+        </form>
+      </section>
     </AdminDashboardLayout>
   );
 };
--- src/components/elements/input/index.tsx
+++ src/components/elements/input/index.tsx
@@ -1,16 +1,20 @@
 import { HTMLAttributes } from "react";
-
+import { twMerge } from "tailwind-merge";
 interface IInputProps extends HTMLAttributes<HTMLInputElement> {
   label: string;
+  className?: string;
 }
 
-const Input = ({ label, ...rest }: IInputProps) => {
+const Input = ({ label, className, ...rest }: IInputProps) => {
   return (
     <>
       <label htmlFor="" className="mr-2">
         {label}
       </label>
-      <input {...rest} className="p-3 text-black border-2 rounded-md" />
+      <input
+        {...rest}
+        className={twMerge(className, "p-3 text-black border-2 rounded-md")}
+      />
     </>
   );
 };
